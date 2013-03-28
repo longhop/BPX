a6b9fa477da50ebec363b9a93ec4b5c1ae82ea47
Michael Matranga <mike@dmgctrl.com>
2013-03-27 22:32:10 -0400

    Added home buttons.

---
+++ BPX1/BPHomeViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface BPHomeViewController : UIViewController
+
+@end
---
+++ BPX1/BPHomeViewController.m
@@ -0,0 +1,24 @@
+#import "BPHomeViewController.h"
+
+@interface BPHomeViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *cookbookButton;
+@property (strong, nonatomic) IBOutlet UIButton *beerButton;
+@property (strong, nonatomic) IBOutlet UIButton *foodButton;
+@property (strong, nonatomic) IBOutlet UIButton *mediaButton;
+
+@end
+
+@implementation BPHomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
